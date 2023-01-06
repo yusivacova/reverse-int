046810fd3e422387d6efc5115606735d009eee24
yusivacova <sivacovayulia@gmail.com>
2023-01-06 19:18:47 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+   let str = n.toString().split("");
+  if (str[0] === '-') {
+     result = str.slice(1).reverse().join("");
+  } else{
+    str = str.reverse().join("");
+    result = str;
+  }
   
+  return result;
 }
